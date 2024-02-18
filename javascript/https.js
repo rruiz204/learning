3353c5b95036515a03e30c55714b2052524897d7
roney ruiz <roney.ruiz@golabstech.com>
2024-02-17 23:02:16 -0600

    javascript - https

---
+++ javascript/https.js
@@ -0,0 +1,12 @@
+console.log("Topic: HTTPS Fetch");
+
+const pokeapi = "https://pokeapi.co/api/v2/pokemon/";
+
+const options = {
+	method: "GET",
+	headers: { "Content-Type": "application/json" }
+}
+
+const response = await fetch(pokeapi, options);
+const data = await response.json();
+console.log(data);
