0642b93254114ef3ad87f73cae460a4b615ed3d6
roney ruiz <roney.ruiz@golabstech.com>
2024-02-18 09:45:10 -0600

    typescript - types

---
+++ typescript/types.ts
@@ -0,0 +1,26 @@
+console.log("Topic: Types");
+
+const integer: number = 100;
+
+const string: string = "Hello World!";
+
+const bool: boolean = true;
+
+
+console.log("Topic: Types with Arrays");
+
+const integers: number[] = [1, 2, 3, 4];
+
+const strings: string[] = ["A", "E", "I", "O", "U"];
+
+const bools: boolean[] = [true, false];
+
+
+console.log("Topic: Tuples");
+
+const tuple: [string, number, boolean] = ["Root", 123, false];
+
+
+console.log("Topic: Any");
+
+const thing: any = 12;
