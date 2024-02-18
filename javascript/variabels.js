b33f15cc18bf4d7bb0d9a95c3948bf377f382975
roney ruiz <roney.ruiz@golabstech.com>
2024-02-17 22:59:31 -0600

    javascript - variables

---
+++ javascript/variabels.js
@@ -0,0 +1,29 @@
+console.log("Topic: Data Types");
+
+let integer = 100;
+
+let string = "Hello World!";
+
+let boolean = true;
+
+let nullable = null || undefined;
+
+
+console.log("Topic: Constants");
+
+const PORT = 3000;
+
+const DB_USER = "admin";
+const DB_PASSWORD = "12345";
+const DB_NAME = "test_db";
+
+
+console.log("Topic: Type Casting");
+
+const INPUT = {
+	user: "some user",
+	email: "email@example.com",
+	age: "27",
+};
+
+console.log(typeof parseInt(INPUT.age));
