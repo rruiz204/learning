dd83f11d7cc66fa9bfc3a82f39dde812ef22de43
roney ruiz <roneyruizrojas@gmail.com>
2024-02-29 23:23:00 -0600

    typescript - statics

---
+++ typescript/static.ts
@@ -0,0 +1,11 @@
+class Calculator {
+  static add(a: number, b:number) {
+    return a + b;
+  }
+
+  static rest(a: number, b:number) {
+    return a - b;
+  }
+}
+
+console.log(Calculator.add(10, 10));
