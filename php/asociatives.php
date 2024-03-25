c7049c9bb69ced358331ea6e74206ec4d87459c3
roney ruiz <roneyruizrojas@gmail.com>
2024-03-24 22:55:16 -0600

    php - work with asociatives

--- php/asociatives.php
+++ php/asociatives.php
@@ -6,3 +6,15 @@ $user = [
   'email'    => 'example@gmail.com',
   'password' => '12345'
 ];
+
+$user['password'] = '12345678';
+print_r($user);
+
+
+echo "Topic: Work with Associative Arrays\n";
+
+$user['role'] = 'developer';
+print_r($user);
+
+unset($user['password']);
+print_r($user);
