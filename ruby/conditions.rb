b52c90fdb4f4fe9a1c7e6fa9f08331b65f35dcba
roney ruiz <roneyruizrojas@gmail.com>
2024-04-01 23:26:17 -0600

    ruby - conditions

---
+++ ruby/conditions.rb
@@ -0,0 +1,16 @@
+puts "Conditions IF - ELIF - ELSE"
+
+number = 123
+
+if (number % 2 == 0)
+  puts "It is an even number"
+  puts "\n"
+else
+  puts "It is an odd number"
+  puts "\n"
+end
+
+puts "Ternary Operator"
+
+boolean = (number % 2 == 0) ? true : false
+puts boolean
