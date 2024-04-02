b89e9b0e4b2027e75c8c3b5e8ada9ac8ac1435dc
roney ruiz <roneyruizrojas@gmail.com>
2024-04-01 23:26:01 -0600

    ruby - variables and data types

---
+++ ruby/variables.rb
@@ -0,0 +1,25 @@
+puts "Variables and Data Types"
+
+integer = 100
+puts integer.class
+
+float = 3.14
+puts float.class
+
+string = "Hello World!"
+puts string.class
+
+boolean = true
+puts boolean.class
+
+null = nil
+puts null.class
+
+array = [100, "Test", true]
+puts array.class
+
+hashe = { username: "Zagreo" }
+puts hashe.class
+
+symbol = :example
+puts symbol.class
