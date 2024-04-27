58de5918c850a5cc581138a6ffee9299e622ad4c
roney ruiz <roneyruizrojas@gmail.com>
2024-04-26 18:27:49 -0600

    dart - variables

---
+++ dart/variables.dart
@@ -0,0 +1,31 @@
+void main() {
+  int age = 20;
+  print(age);
+
+  double price = 3000.50;
+  print(price);
+
+  bool maybe = true;
+  print(maybe);
+
+  String name = "some name";
+  print(name);
+
+  List<int> integers = [1, 2, 3 , 4];
+  print(integers);
+
+  List<String> strings = ["a", "b", "c"];
+  print(strings);
+
+  Map<String, dynamic> character = {
+    "name": "Example",
+    "age": 19,
+  };
+  print(character);
+
+  const pi = 3.14;
+  print(pi);
+
+  final date = DateTime(1990, 5, 15);
+  print(date);
+}
