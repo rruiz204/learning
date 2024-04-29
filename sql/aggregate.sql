99d603694f923ee732da57f3ddd48a30509978fb
roney ruiz <roneyruizrojas@gmail.com>
2024-04-28 18:54:14 -0600

    sql - delete & aggregate functions

---
+++ sql/aggregate.sql
@@ -0,0 +1,12 @@
+-- Min and Max Functions
+SELECT MIN(column) AS smallest FROM some_table;
+SELECT MAX(column) AS biggest FROM some_table;
+
+-- Count Function
+SELECT COUNT(column) AS result FROM some_table;
+
+-- Sum Function
+SELECT SUM(column) AS result FROM some_table;
+
+-- Avg Function
+SELECT AVG(column) AS average FROM some_table;
---
+++ sql/delete.sql
@@ -0,0 +1,5 @@
+-- Specific Delete
+DELETE FROM some_table WHERE column = some_data;
+
+-- General Delete WARNING
+DELETE FROM some_table;
