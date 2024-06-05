6b8e14d701d82e86312041368be8c23fa68d5af5
roney ruiz <roneyruizrojas@gmail.com>
2024-06-05 11:49:13 -0600

    loops in java

---
+++ javas/Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+  public static void main(String[] args) {
+    for (int index = 0; index < 10; index++) {
+      System.out.println(index);
+    }
+
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    for (String car : cars) {
+      System.out.println(car);
+    }
+  }
+}
