1f8b9b2c4bd12bcdd1cd00c5e33d1924d37d3582
roney ruiz <roneyruizrojas@gmail.com>
2024-06-05 11:48:44 -0600

    variables in java

---
+++ javas/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+  public static void main(String[] args) {
+    String text = "Some text";
+    System.out.println(text);
+
+    int number = 100;
+    System.out.println(number);
+
+    double pi = 3.14;
+    System.out.println(pi);
+
+    boolean maybe = true;
+    System.out.println(maybe);
+  }
+}
