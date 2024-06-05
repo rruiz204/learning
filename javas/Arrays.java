969bbfb5f5b0d2fba4407cd1a4dfdff762d91e45
roney ruiz <roneyruizrojas@gmail.com>
2024-06-05 11:49:20 -0600

    arrays in java

---
+++ javas/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+  public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    System.out.println(cars[1]);
+
+    int[] numbers = {10, 20, 30, 40};
+    System.out.println(numbers[3]);
+
+    numbers[3] = 90;
+    System.out.println(numbers[3]);
+  }
+}
